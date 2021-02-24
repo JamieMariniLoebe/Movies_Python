48acaaf0e161b3a5f7a677c935e30a68b8cd21c2
Jamie Loebe <jamie.loebe2@gmail.com>
2021-02-24 14:45:23 -0600

    Updated user input func, implemented option 1

--- Movies.py
+++ Movies.py
@@ -1,38 +1,70 @@
 import csv
 import time
 
-
-def valid_input(input):
+# Source for csv reading
+# https://stackoverflow.com/questions/56342198/python-code-to-read-csv-file-based-on-user-input
+def readCSV():
     while True:
-        # See whether input is valid or not
         try:
-            userInput = int(input)
-        # Try again if invalid input
-        except ValueError:
-            print("ERROR! Input is not an integer, please type a valid option")
-            continue
-        # If valid, break from loop
-        else:
-            break
+            file = str(input("\nEnter file you want to use: "))
+            if not ".csv" in file:
+                file += ".csv"
+        except Exception as e:
+            print(e)
 
-def option_chosen(input):
+# Display menu options
+def menu():
+    print("Please choose from the below options:")
+    print("1 - Show movies released in a specific year")
+    print("2 - Show highest rated movie for each year")
+    print("3 - Show movies and their release year for a specific language")
+    print("4 - Exit\n")
+
+# Display data requested by user
+def display_data(user_input):
+    num_movies = 0
     
-    if(input == 1):
-        print("Show movies released in a specific year")
+    if(user_input == 1):
+        user_year = input("\nSee movies released in what year? ")
+        print("")
+
+        for film in movies_list:
+            if film.year == user_year:
+                print(film.title)
+                num_movies += 1
+            else:
+                continue
+        
+        print("")
+        if(num_movies < 1):
+            print("There were no movies released in ", user_year)
+            print("")
     
-    elif(input == 2):
-        print("Show highest rated mnovie in specific year")
+    elif(user_input == 2):
+        print("\nShow highest rated mnovie in specific year")
+        return 0
 
-    elif(input == 3):
-        print("Show movie+year for specific langauges")
+    elif(user_input == 3):
+        print("\nShow movie+year for specific langauges")
+        return 0
 
-    elif(input == 4):
-        print("Exit chosen!")
+    elif(user_input == 4):
+        print("\nExiting program....")
+        exit()
 
+# Get user input for menu option, loop for validation
+# Source: https://medium.com/better-programming/how-to-indefinitely-request-user-input-until-valid-in-python-388a7c85aa6e
+def getInput():
+    while True:
+        try:
+            userChoice = int(input("Enter number of option selected: ")) 
+            if userChoice > 0 and userChoice < 5:
+                return userChoice
+            print("Error! Invalid option, please try again\n")
+        except Exception as e:
+            print(e)
 
 # Create class to store films
-
-
 class Films():
     title = ""
     year = 0
@@ -46,18 +78,19 @@ class Films():
         self.rating = var4
 
 
-print("Hello!")
 
-# Source for csv reading
-# https://stackoverflow.com/questions/56342198/python-code-to-read-csv-file-based-on-user-input
-file = str(input("Enter file you want to use: "))
-if not ".csv" in file:
-    file += ".csv"
 
-# print(file)
+
+
+# Read in csv file
+
+
+
+# Parse csv file into objects to handle data
 
 # Source for parsing csv file into objects
 # https://stackoverflow.com/questions/31933257/python-3-how-to-read-a-csv-file-and-store-specific-values-as-variables
+
 movies_list = []
 
 with open(file, newline='') as csv_file:
@@ -65,23 +98,21 @@ with open(file, newline='') as csv_file:
     for row in movies_file:
         movies_list.append(Films(row[0], row[1], row[2], row[3]))
 
-# print(movies_list[5].title)
 
 time.sleep(1)
 
-print("\nGreat! I've read your file and parsed the data!\n")
+print("\nGreat, I've read your file and parsed the data!\n")
 
 time.sleep(1)
 
-print("Please choose from the below options:")
-print("1 - Show movies released in a specific year")
-print("2 - Show highest rated movie for each year")
-print("3 - Show movies and their release year for a specific language")
-print("4 - Exit\n")
+while True:
+    # Print menu (use function)
+    menu()
 
-userChoice = int(input("Enter number of option selected: "))
-valid_input(userChoice)
-print("")
-option_chosen(userChoice)
+    # Get user input for menu option, loop for validation
+    userInput = getInput()
+
+    # Display data requested by user
+    display_data(userInput)
 
 
