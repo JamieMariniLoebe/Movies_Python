936d8c9c7ed2be67757984ce3985c656e9066b96
Jamie Loebe <jamie.loebe2@gmail.com>
2021-02-25 14:21:27 -0600

    Updated functionality for options 1 & 2

--- Movies.py
+++ Movies.py
@@ -1,16 +1,39 @@
 import csv
 import time
+import os
 
 # Source for csv reading
 # https://stackoverflow.com/questions/56342198/python-code-to-read-csv-file-based-on-user-input
 def readCSV():
+    movies_list = []
+
     while True:
         try:
             file = str(input("\nEnter file you want to use: "))
+
+            # Add file extension '.csv' if not included
             if not ".csv" in file:
                 file += ".csv"
-        except Exception as e:
-            print(e)
+            
+            # Bool to check if file exists
+            exists = os.path.isfile(file)
+
+            if(exists == True):
+                #print(exists)
+                break
+            else:
+                print("Error! File not found, please try again\n")
+        except IOError:
+            print("File not found")
+
+    #Open file, and parse data
+    with open(file, newline='') as csv_file:
+        movies_file = csv.reader(csv_file)
+        for row in movies_file:
+            movies_list.append(Films(row[0], row[1], row[2], row[3]))
+        
+    return movies_list
+
 
 # Display menu options
 def menu():
@@ -21,17 +44,19 @@ def menu():
     print("4 - Exit\n")
 
 # Display data requested by user
-def display_data(user_input):
+def display_data(user_input, movies):
     num_movies = 0
     
+    #Display movies released in user specified year
     if(user_input == 1):
         user_year = input("\nSee movies released in what year? ")
         print("")
 
-        for film in movies_list:
+        # Iterate thru movies, display films released in user specified year
+        for film in movies:
             if film.year == user_year:
                 print(film.title)
-                num_movies += 1
+                num_movies += 1 # counter to track # of movies
             else:
                 continue
         
@@ -39,10 +64,25 @@ def display_data(user_input):
         if(num_movies < 1):
             print("There were no movies released in ", user_year)
             print("")
-    
+
+    # Display highest rated movie(s) in user specified year
     elif(user_input == 2):
-        print("\nShow highest rated mnovie in specific year")
-        return 0
+        #print("\nShow highest rated movie in specific year")
+
+        high_rated = []
+
+        year = input("Please enter which year you would like to see the 3 highest rated 3 movies: ")
+
+        # Append all movies released in user specified year to list 'high_rated'
+        for film in movies:
+            if(film.year == year):
+                high_rated.append(film)
+            else:
+                continue
+        
+        highest_rated = sorted(high_rated, key=lambda films: films.rating)
+
+        print(highest_rated)
 
     elif(user_input == 3):
         print("\nShow movie+year for specific langauges")
@@ -91,13 +131,7 @@ class Films():
 # Source for parsing csv file into objects
 # https://stackoverflow.com/questions/31933257/python-3-how-to-read-a-csv-file-and-store-specific-values-as-variables
 
-movies_list = []
-
-with open(file, newline='') as csv_file:
-    movies_file = csv.reader(csv_file)
-    for row in movies_file:
-        movies_list.append(Films(row[0], row[1], row[2], row[3]))
-
+movies = readCSV()
 
 time.sleep(1)
 
@@ -113,6 +147,6 @@ while True:
     userInput = getInput()
 
     # Display data requested by user
-    display_data(userInput)
+    display_data(userInput, movies)
 
 
